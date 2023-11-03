afdf174c3973e7d4c3ff1de853296c73cdf5ebc5
ariannagutirrzz <ariannavega2021@gmail.com>
2023-11-03 18:49:00 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline bg-red-900'>
+       probando tailwind
+      </div>
+      
+    </>
+  )
+}
+
+export default App

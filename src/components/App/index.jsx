9592e65a4c03092619e454c73ae35b2f4e1f94fb
ariannagutirrzz <ariannavega2021@gmail.com>
2023-11-04 02:44:12 -0400

    BrowserRouter

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,26 +1,38 @@
-import '../App/App.css'
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import React from 'react'
+
 import MyAccount from '../MyAccount/index'
 import MyOrder from '../MyOrder/index'
 import MyOrders from '../MyOrders/index'
 import SignUp from '../SignUp/index'
 import NotFound from '../NotFound/index'
 import Home from '../Home/index'
+import '../App/App.css'
 
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home/>},
+    { path: '/my-order', element: <MyOrder/> },
+    { path: '/my-orders', element: <MyOrders/> },
+    { path: '/my-account', element: <MyAccount/> },
+    { path: '/*', element: <NotFound/> },
+    { path: '/sign-up', element: <SignUp/> }
+  ])
 
-function App() {
+  return routes
+}
+
+const App = () => {
 
   return (
     <>
       <div className='text-3xl font-bold underline bg-red-900'>
        folders
       </div>
-      <Home/>
-      <MyOrder/>
-      <MyOrders/>
-      <SignUp/>
-      <MyAccount/>
-      <NotFound/>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+
     </>
   )
 }

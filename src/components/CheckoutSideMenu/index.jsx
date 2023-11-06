0becec0e38e6bad247a780d7ba6454932a79fd3b
ariannagutirrzz <ariannavega2021@gmail.com>
2023-11-06 00:54:25 -0400

    Order card component

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -2,7 +2,7 @@ import "./styles.css";
 import closeIcon from "../../assets/x.svg";
 import { useContext } from "react";
 import { CartContext } from "../Context/Context";
-
+import OrderCard from "../OrderCard";
 const CheckoutSideMenu = () => {
   const context = useContext(CartContext);
   console.log("effe:" + context.productToShow)
@@ -11,13 +11,24 @@ const CheckoutSideMenu = () => {
     <aside
       className={`${
         context.isCheckoutMenuOpen ? "flex" : "hidden"
-      } checkout-side-menu flex-col fixed right-0 border bg-white border-black rounded-lg >`}
+      } checkout-side-menu scrollable-cards flex-col fixed right-0 border bg-white border-black rounded-lg >`}
     >
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">My order</h2>
         <div>
           <img className="cursor-pointer" onClick={context.closeCheckoutMenu} src={closeIcon} alt="close-icon" />
         </div>
+      </div>
+      <div className="px-6">
+      {context.cartProducts.map(product => (
+        <OrderCard 
+        key={product.id}
+        title={product.title}
+        image={product.image}
+        price={product.price}
+        />
+      ))
+      }
       </div>
     </aside>
   );
---
+++ src/components/OrderCard/index.jsx
@@ -0,0 +1,21 @@
+import icon from "../../assets/x.svg"
+
+const OrderCard = props => {
+    const {id,title,image,price} = props;
+  return (
+    <div className="flex justify-between items-center mb-4">
+      <div className="flex items-center gap-2">
+        <figure className="w-20 h-20">
+          <img className="w-full h-full rounded-lg object-cover truncate" src={image} alt={title} />
+        </figure>
+        <p className="text-sm font-light">{title}</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <p className="text-lg font-medium">${price}</p>
+        <img className="h-6 w-6 cursor-pointer" src={icon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default OrderCard;

34db6fa5688c67628d657256142b6149dfb29a01
ariannagutirrzz <ariannavega2021@gmail.com>
2023-11-08 16:28:16 -0400

    Styles for orders

--- src/components/OrdersCard/index.jsx
+++ src/components/OrdersCard/index.jsx
@@ -4,11 +4,13 @@ const OrdersCard = (props) => {
   const { totalPrice, totalProducts } = props;
 
   return (
-    <div className="flex justify-between items-center mb-4 border border-black">
-      <p>
-        <span>01.02.23</span>
-        <span>{totalProducts}</span>
-        <span>{totalPrice}</span>
+    <div className="flex justify-between items-center mb-4 border border-black w-80 p-4 rounded-lg ">
+      <p className="flex justify-between w-full">
+       <div className="flex flex-col font-light">
+       <span>01.02.23</span>
+        <span>{totalProducts} articles </span>
+       </div>
+        <span className="font-medium text-2xl">${totalPrice} </span>
       </p>
     </div>
   );
